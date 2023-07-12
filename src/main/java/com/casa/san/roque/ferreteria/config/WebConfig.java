7dc6b7547cb1aef86e65946302c953c33b0b639d
MatiasCasco <matiascasco_96@hotmail.com>
2023-07-11 21:13:58 -0400

    Se agrego webconfig

---
+++ src/main/java/com/casa/san/roque/ferreteria/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.casa.san.roque.ferreteria.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author Matias Casco
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+    }
+    
+}
